9383224a1fae8fc5ca371d10228d0fca7dc38ec7
jgravesjr <jgravesjr@outlook.com>
2017-04-06 12:25:22 -0400

    Installed Bootstrap Template

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="_/css/styles.min.css" rel="stylesheet" media="screen">
+
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="_/js/bootstrap.min.js"></script>
+    <script src="_/js/scrips.min.js"></script>
+  </body>
+</html>
